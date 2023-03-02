1aa907d46ec76c2c9e70da03b2df6d71db6b8202
ViktorBalko <viktorbalko.samsung@gmail.com>
2023-03-02 05:57:57 +0200

    020322

--- js/app.js
+++ js/app.js
@@ -1,63 +1,121 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+  wrapper: ".wrapper",
+  content: ".content",
+  smooth: 1.5,
+  effects: true
+});
 
-if (ScrollTrigger.isTouch !== 1) {
-  ScrollSmoother.create({
-    wrapper: ".wrapper",
-    content: ".content",
-    smooth: 1.5,
-    effects: true
-  });
+gsap.fromTo(
+  ".hero-section",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".hero-section",
+      start: "center",
+      end: "750",
+      //   end: "bottom",
+      scrub: true
+    }
+  }
+);
 
+let itemsLeft = gsap.utils.toArray(".carousel__left .carousel__item");
+
+itemsLeft.forEach((item) => {
   gsap.fromTo(
-    ".hero-section",
-    { opacity: 1 },
+    item,
+    { opacity: 0, x: -130 },
     {
-      opacity: 0,
+      opacity: 1,
+      x: 0,
       scrollTrigger: {
-        trigger: ".hero-section",
-        start: "center",
-        end: "850",
-        //   end: "bottom",
+        trigger: item,
+        start: "-900",
+        end: "top",
         scrub: true
       }
     }
   );
+});
 
-  let itemsLeft = gsap.utils.toArray(".carousel__left .carousel__item");
+let itemsRight = gsap.utils.toArray(".carousel__right .carousel__item");
 
-  itemsLeft.forEach((item) => {
-    gsap.fromTo(
-      item,
-      { opacity: 0, x: -80 },
-      {
-        opacity: 1,
-        x: 0,
-        scrollTrigger: {
-          trigger: item,
-          start: "-900",
-          end: "top",
-          scrub: true
-        }
+itemsRight.forEach((item) => {
+  gsap.fromTo(
+    item,
+    { opacity: 0, x: 120 },
+    {
+      opacity: 1,
+      x: 0,
+      scrollTrigger: {
+        trigger: item,
+        start: "-900",
+        end: "top",
+        scrub: true
       }
-    );
-  });
+    }
+  );
+});
+// if (ScrollTrigger.isTouch !== 1) {
+//   ScrollSmoother.create({
+//     wrapper: ".wrapper",
+//     content: ".content",
+//     smooth: 1.5,
+//     effects: true
+//   });
 
-  let itemsRight = gsap.utils.toArray(".carousel__right .carousel__item");
+//   gsap.fromTo(
+//     ".hero-section",
+//     { opacity: 1 },
+//     {
+//       opacity: 0,
+//       scrollTrigger: {
+//         trigger: ".hero-section",
+//         start: "center",
+//         end: "750",
+//         //   end: "bottom",
+//         scrub: true
+//       }
+//     }
+//   );
 
-  itemsRight.forEach((item) => {
-    gsap.fromTo(
-      item,
-      { opacity: 0, x: 80 },
-      {
-        opacity: 1,
-        x: 0,
-        scrollTrigger: {
-          trigger: item,
-          start: "-900",
-          end: "top",
-          scrub: true
-        }
-      }
-    );
-  });
-}
+//   let itemsLeft = gsap.utils.toArray(".carousel__left .carousel__item");
+
+//   itemsLeft.forEach((item) => {
+//     gsap.fromTo(
+//       item,
+//       { opacity: 0, x: -80 },
+//       {
+//         opacity: 1,
+//         x: 0,
+//         scrollTrigger: {
+//           trigger: item,
+//           start: "-900",
+//           end: "top",
+//           scrub: true
+//         }
+//       }
+//     );
+//   });
+
+//   let itemsRight = gsap.utils.toArray(".carousel__right .carousel__item");
+
+//   itemsRight.forEach((item) => {
+//     gsap.fromTo(
+//       item,
+//       { opacity: 0, x: 80 },
+//       {
+//         opacity: 1,
+//         x: 0,
+//         scrollTrigger: {
+//           trigger: item,
+//           start: "-900",
+//           end: "top",
+//           scrub: true
+//         }
+//       }
+//     );
+//   });
+// }
